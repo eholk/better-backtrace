39ad5f07c63a6acbfb7d5049f45eacda2221f873
Eric Holk <ericholk@microsoft.com>
2021-12-15 14:56:37 -0800

    Recreate the usual backtrace printing

---
+++ examples/simple-backtrace.rs
@@ -0,0 +1,5 @@
+use better_backtrace::print_backtrace;
+
+fn main() {
+    print_backtrace();
+}
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub fn print_backtrace() {
+    let mut index = 0;
+    backtrace::trace(|frame| {
+        backtrace::resolve_frame(frame, |sym| {
+            println!("{:2}: {}", index, sym.name().expect("no symbol name"));
+            if let Some(filename) = sym.filename() {
+                println!("\tat {}:{}", filename.display(), sym.lineno().unwrap())
+            }
+        });
+        index += 1;
+        true
+    });
+}
