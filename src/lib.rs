224f32f812e79c0e2eff252251446e6bc558f3ba
Eric Holk <ericholk@microsoft.com>
2022-01-06 16:30:41 -0800

    Match existing formatting rules and add support for filtering based on function prefix

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,87 @@
 use std::{io::Write, str::FromStr};
 
+use backtrace::Symbol;
+
 pub fn print_backtrace() {
     format_backtrace(&BacktraceConfig::default(), std::io::stdout())
 }
 
 fn format_backtrace(config: &BacktraceConfig, mut out: impl Write) {
+    let trace = collect_backtrace();
     let mut index = 0;
-    backtrace::trace(|frame| {
-        backtrace::resolve_frame(frame, |sym| {
-            writeln!(out, "{:2}: {}", index, sym.name().expect("no symbol name")).unwrap();
-            if let Some(filename) = sym.filename() {
-                writeln!(out, "\tat {}:{}", filename.display(), sym.lineno().unwrap()).unwrap();
+    let mut show_frames = match config.style {
+        BacktraceStyle::None => false,
+        BacktraceStyle::Short => !trace.contains_short_end,
+        BacktraceStyle::Full => true,
+    };
+    for (index, frame) in trace.frames.iter().enumerate() {
+        if matches!(config.style, BacktraceStyle::Short) {
+            if frame.name.contains("__rust_begin_short_backtrace") {
+                show_frames = false;
             }
-            index += 1;
-        });
+        }
+        if show_frames && config.filter.should_display_frame(&frame.name) {
+            writeln!(out, "{:2}: {}", index, frame.name).unwrap();
+            if let Some((filename, line)) = &frame.file_position {
+                writeln!(out, "\tat {}:{}", filename, line).unwrap();
+            }
+        }
+        if matches!(config.style, BacktraceStyle::Short) {
+            if frame.name.contains("__rust_end_short_backtrace") {
+                show_frames = true;
+            }
+        }
+    }
+}
+
+fn collect_backtrace() -> Backtrace {
+    let mut frames = vec![];
+    let mut contains_short_end = false;
+    backtrace::trace(|frame| {
+        let info = FrameInfo::from_frame(frame);
+        if info.name.contains("__rust_end_short_backtrace") {
+            contains_short_end = true;
+        }
+        frames.push(info);
         true
     });
+    Backtrace {
+        frames,
+        contains_short_end,
+    }
+}
+
+struct Backtrace {
+    frames: Vec<FrameInfo>,
+    contains_short_end: bool,
+}
+
+struct FrameInfo {
+    name: String,
+    /// (filename, line number)
+    file_position: Option<(String, usize)>,
+}
+
+impl FrameInfo {
+    fn from_frame(frame: &backtrace::Frame) -> Self {
+        let mut name = "<unknown>".to_string();
+        let mut file_position = None;
+        backtrace::resolve_frame(frame, |sym| {
+            name = sym
+                .name()
+                .map_or_else(|| "<unknown>".to_string(), |name| name.to_string());
+            if let Some(filename) = sym.filename() {
+                file_position = Some((
+                    format!("{}", filename.display()),
+                    sym.lineno().unwrap() as usize,
+                ))
+            }
+        });
+        Self {
+            name,
+            file_position,
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -23,7 +89,7 @@ struct Pattern(String);
 
 impl Pattern {
     fn matches(&self, frame: &str) -> bool {
-        frame.contains(&self.0)
+        frame.starts_with(&self.0)
     }
 }
 
@@ -94,7 +160,7 @@ impl Default for BacktraceConfig {
     fn default() -> Self {
         Self {
             style: BacktraceStyle::Short,
-            filter: Filter(vec![]),
+            filter: Filter(vec![FilterClause::Exclude(Pattern("backtrace::".into()))]),
         }
     }
 }
